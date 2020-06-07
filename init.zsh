1ad9f724eb725f8d3911e7c3c4e654eba6a23787
François VANTOMME <akarzim@gmail.com>
2020-06-07 20:03:31 +0200

    Chore: add a .zsh symlink

--- init.zsh
+++ init.zsh
@@ -1,23 +1,23 @@
 # Module Loader
 
 # This module must be loaded first
-source "${0:h}/modules/environment.zsh"
-source "${0:h}/modules/directory.zsh"
-source "${0:h}/modules/editor.zsh"
+source "${0:h}/.zsh/modules/environment.zsh"
+source "${0:h}/.zsh/modules/directory.zsh"
+source "${0:h}/.zsh/modules/editor.zsh"
 
-source "${0:h}/modules/git.zsh"
+source "${0:h}/.zsh/modules/git.zsh"
 
 # This module must be loaded before the history-substring-search module
-source "${0:h}/modules/history.zsh"
-source "${0:h}/modules/osx.zsh"
-source "${0:h}/modules/ruby.zsh"
-source "${0:h}/modules/rails.zsh"
+source "${0:h}/.zsh/modules/history.zsh"
+source "${0:h}/.zsh/modules/osx.zsh"
+source "${0:h}/.zsh/modules/ruby.zsh"
+source "${0:h}/.zsh/modules/rails.zsh"
 
-source "${0:h}/modules/ssh.zsh"
-source "${0:h}/modules/history-substring-search.zsh"
+source "${0:h}/.zsh/modules/ssh.zsh"
+source "${0:h}/.zsh/modules/history-substring-search.zsh"
 
-source "${0:h}/modules/helper.zsh"
-source "${0:h}/modules/spectrum.zsh"
-source "${0:h}/modules/utility.zsh"
-source "${0:h}/modules/completion.zsh"
-source "${0:h}/modules/nvm.zsh"
+source "${0:h}/.zsh/modules/helper.zsh"
+source "${0:h}/.zsh/modules/spectrum.zsh"
+source "${0:h}/.zsh/modules/utility.zsh"
+source "${0:h}/.zsh/modules/completion.zsh"
+source "${0:h}/.zsh/modules/nvm.zsh"
